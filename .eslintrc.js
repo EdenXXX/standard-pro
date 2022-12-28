38dec582a4f037878a69294fc0152987b05f0ccf
Eden <rongrong.xiao@emeet.ai>
2022-12-28 09:38:32 +0800

    build: 安装Stylelint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@
  * @Description:
  * @Author: Eden
  * @Date: 2022-12-23 15:06:57
- * @LastEditTime: 2022-12-26 17:44:12
+ * @LastEditTime: 2022-12-27 16:33:14
  * @LastEditors: Eden
  */
 module.exports = {
@@ -24,7 +24,6 @@ module.exports = {
   plugins: ['react'],
   rules: {
     // semi: 0,
-
     'prettier/prettier': [
       'off',
       {
---
+++ .stylelintrc.js
@@ -0,0 +1,10 @@
+/*
+ * @Description:
+ * @Author: Eden
+ * @Date: 2022-12-27 16:36:37
+ * @LastEditTime: 2022-12-27 17:44:14
+ * @LastEditors: Eden
+ */
+module.exports = {
+  extends: ['stylelint-config-standard', 'stylelint-config-prettier']
+}
